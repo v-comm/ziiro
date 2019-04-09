f0439611355e9115e782d1e04961a7c49b973505
Y ∆ Z U I™ <36424983+yazui@users.noreply.github.com>
2019-04-09 12:56:09 -0300

    v0.1

---
+++ header.php
@@ -0,0 +1,119 @@
+<head>
+    <!-- title ..................................... -->
+    <title>V-COMM</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="vcomm/favicon.png">
+
+    <!-- facebook ..................................... -->
+    <meta property="og:url"           content="https://www.vcomm.com.br" />
+    <meta property="og:type"          content="website" />
+    <meta property="og:title"         content="V-COMM - Virtual Commerce" />
+    <meta property="og:description"   content="V-Comm é uma plataforma de Marketing Digital que ajuda você a divulgar sua empresa na Internet." />
+    <meta property="og:image"         content="facebook.jpg" />
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- css ..................................... -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.3/dist/jquery.fancybox.min.css"/>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Montserrat:200,400,700,900');
+        body{
+            font-family: 'Montserrat', sans-serif !important;
+        }
+        .label{
+            color: salmon !important;
+        }
+        .info{
+            color: #666 !important;
+            font-weight: 400 !important;
+            font-size: 12px;
+        }
+        .fine{
+            font-weight: 200 !important;
+        }
+        /* ..................................... 
+
+        default colors
+
+        ..................................... */
+
+        :root {
+            --color1: #fa8072;
+            --color2: #BB7365;
+            --color3: #DEADA1;
+            --color4: #ffffff;
+            --color5: #3C4663;
+            --color6: #6D748C;
+        }
+        ::selection {      background: var(--color1); }
+        ::-moz-selection { background: var(--color1); }
+
+        a{
+            color: var(--color5) !important;
+            font-weight: bold;
+            transition: color .5s ease-out;
+            -moz-transition: color .5s ease-out;
+            -webkit-transition: color .5s ease-out;
+            -o-transition: color .5s ease-out;
+        }
+        a:hover{ color: var(--color1) !important; }
+
+        .gray{
+            background-color: #f2f2f2;
+            color: #f2f2f2;
+        }
+        .color1{ color: var(--color1) !important; }
+        .color2{ color: var(--color2) !important; }
+        .color3{ color: var(--color3) !important; }
+        .color4{ color: var(--color4) !important; }
+        .color5{ color: var(--color5) !important; }
+        .color6{ color: var(--color6) !important; }
+        .black{ color: black !important;}
+
+        .bg1{ background-color: var(--color1) !important; }
+        .bg2{ background-color: var(--color2) !important; }
+        .bg3{ background-color: var(--color3) !important; }
+        .bg4{ background-color: var(--color4) !important; }
+        .bg5{ background-color: var(--color5) !important; }
+        .bg6{ background-color: var(--color6) !important; }
+
+        .is-color{
+            background-color: var(--color1);
+            color: var(--color4) !important;
+            border: 0 !important;
+            font-family: 'Montserrat', sans-serif;
+        }
+        .is-color:hover{
+            color: var(--color6) !important;
+            background-color: var(--color5) !important;
+        }
+        .is-color:focus{
+            color: var(--color1) !important;
+            background-color: #f2f2f2 !important;
+        }
+        .is-color h1{
+            color: white !important;
+        }
+
+    </style>
+
+    <!-- js ..................................... -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.3/dist/jquery.fancybox.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,92 @@
+<html>
+    <!-- header -->
+    <?php include('header.php'); ?>
+
+    <!-- content -->
+    <body>
+        <form>
+            <section class="hero is-light is-bold">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="subtitle is-5">Etapa 01/02</h1>
+                        <h1 class="title is-1 fine">Configure seu anúncio</h1>
+
+                        <!-- etapa 01 -->
+                        <div class="columns">
+                            <div class="column">
+                                <video src="http://site.vcomm.com.br/arts/motion/01.mp4" controls muted autoplay loop></video>
+                            </div>
+                            <div class="column">
+                                <input type="text" value="motion 01" hidden>
+                                <label class="label">Chamada 01</label>
+                                <textarea class="textarea" rows="2" required></textarea>
+                                <p>&nbsp;</p>
+                                <label class="label">Chamada 02</label>
+                                <textarea class="textarea" rows="2" required></textarea>
+                                <p>&nbsp;</p>
+                                <label class="label">Sua Logo</label>
+                                <small class="info">OBS: PNG sem fundo.</small>
+                                <div class="box">
+                                    <input type="file" name="image1" required>
+                                </div>
+
+                                <div class="columns">
+                                    <div class="column">
+                                        <label class="label">Fotografia 01</label>
+                                        <div class="box">
+                                            <input type="file" name="image1" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="column">
+                                        <label class="label">Fotografia 02</label>
+                                        <div class="box">
+                                            <input type="file" name="image1" required>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <label class="label">Quando o cliente clicar no anúncio, enviar ele para:</label>
+                        <small class="info">Se for WhatsApp, por favor insira o número</small>
+                        <input class="input" required>
+                        <p>&nbsp;</p>
+
+                        <a class="button is-color" href="#pagamento">Pronto. Configurei meu anúncio.</a>
+                    </div>
+                </div>
+            </section>
+
+            <section class="hero is-fullheight bg5" id="pagamento">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="subtitle is-5 color3">Etapa 02/02</h1>
+                        <h1 class="title is-1 fine color2">Seus dados</h1>
+
+                        <div class="columns">
+                            <div class="column">
+                                <label class="label">Empresa</label>
+                                <input type="text" class="input" required>
+                                <p>&nbsp;</p>
+                                <label class="label">E-Mail</label>
+                                <input type="text" class="input" required>
+                                <p>&nbsp;</p>
+                            </div>
+
+                            <div class="column">
+                                <label class="label">Representante da Empresa</label>
+                                <input type="text" class="input" required>
+                                <p>&nbsp;</p>
+                                <label class="label">Telefone de Contato</label>
+                                <input type="text" class="input" required>
+                                <p>&nbsp;</p>
+                            </div>
+                        </div>
+
+                        <button class="button is-color" type="submit">Anunciar!</button>
+                    </div>
+                </div>
+            </section>
+        </form>
+    </body>
+</html>
